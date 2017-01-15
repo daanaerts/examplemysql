432611f9d6d28531b8bfc906e0fae1d6b1ccc2d1
daanaerts <daanaerts@gmail.com>
2017-01-15 14:46:14 +0100

    added read stuff

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
 'use strict';
 
-console.log('Hello! sf sdf sdf');
+console.log('working server');
+
+const mysql = require('mysql');
+const app = require('express')();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'my-secret-pw',
+  database: 'exampleeeee'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(error, results, fields) {
+  if (error) throw error;
+  console.log('MySQL connected');
+});
+
+app.get('', (request, response) => {
+  response.status(200).json({ message: 'ok' });
+});
+
+app.get('/todos', (request, response) => {
+  connection.query('SELECT * FROM todos', (error, data) => {
+    if (error) {
+      return response.status(500).json(error);
+    }
+    response.status(200).json({ status: 200, data: data });
+    console.log(response);
+  });
+});
+
+const server = require('http').createServer(app);
+server.listen(3000);
+
+console.log(`server listening on port ${server.address().port}`)
