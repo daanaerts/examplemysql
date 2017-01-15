318372e451598f2d1d76a83e1d00bec2f1d9b3d7
Jack Sacali <jacksacali@gmail.com>
2017-01-15 15:22:32 +0100

    Update added to todos

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app = require('express')();
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'my-secret-pw',
-  database: 'exampleeeee'
+  password: '2017',
+  database: 'hyf'
 });
 
 connection.connect();
@@ -29,6 +29,17 @@ app.get('/todos', (request, response) => {
       return response.status(500).json(error);
     }
     response.status(200).json({ status: 200, data: data });
+  });
+});
+
+// Update the title where the id is 10
+app.patch('/todos', (request, response) => {
+  connection.query('UPDATE todos SET title="updated title", updatedAt = CURRENT_TIMESTAMP WHERE todoId=10', (error, data) => {
+    if (error) {
+      return response.status(500).json(error);
+    }
+    response.status(200).json({ status: 200, data: data });
+    console.log('Title updated');
   });
 });
 
