0c2102fb49cc2758e134c59a10a1a4dd7eb807ee
daanaerts <daanaerts@gmail.com>
2017-01-17 15:59:41 +0100

    externalized controllers, database and added database config to gitignore

--- index.js
+++ index.js
@@ -2,39 +2,11 @@
 
 console.log('working server');
 
-const mysql = require('mysql');
 const app = require('express')();
+require('./lib/controllers')(app);
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'my-secret-pw',
-  database: 'exampleeeee'
-});
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(error, results, fields) {
-  if (error) throw error;
-  console.log('MySQL connected');
-});
-
-app.get('', (request, response) => {
-  response.status(200).json({ message: 'ok' });
-});
-
-app.get('/todos', (request, response) => {
-  connection.query('SELECT * FROM todos', (error, data) => {
-    if (error) {
-      return response.status(500).json(error);
-    }
-    response.status(200).json({ status: 200, data: data });
-  });
-});
 
 const server = require('http').createServer(app);
 server.listen(3000);
 
 console.log(`server listening on port ${server.address().port}`)
-
-console.log('I am a change');
